e8b6173b1068e0130c3bf73992ed3f0be6ad63e1
Joey ter Meulen <joeytermeulen@gmail.com>
2023-04-06 23:20:10 +0200

    chore: add support for doc stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,23 @@
 const path = require("path");
 
 module.exports = {
-  stories: ["../src/**/*.stories.tsx"],
+  stories: ["../src/**/*.stories.tsx", "../src/docs/*.stories.mdx"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   framework: {
     name: "@storybook/react-webpack5",
     options: {}
   },
   docs: {
-    autodocs: true
+    autodocs: true,
+    hideNoControlsWarning: true,
   },
   webpackFinal: async (config) => {
-    // Make whatever fine-grained changes you need
     config.module.rules.push({
       test: /\.scss$/,
       use: ["style-loader", "css-loader", "sass-loader"],
       include: path.resolve(__dirname, "../"),
     });
 
-    // Return the altered config
     return config;
   }
 };

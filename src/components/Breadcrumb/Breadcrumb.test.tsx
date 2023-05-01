5f3b710e6ced897dd7f4ade0b59309a13ca9fddc
Joey ter Meulen <joeytermeulen@gmail.com>
2023-05-01 17:32:27 +0200

    test: add snapshot for breadcrumb

--- src/components/Breadcrumb/Breadcrumb.test.tsx
+++ src/components/Breadcrumb/Breadcrumb.test.tsx
@@ -4,9 +4,12 @@ import { render } from "@testing-library/react";
 import Breadcrumb from "./Breadcrumb";
 
 describe("Breadcrumb", () => {
-    // TODO: add tests for Breadcrumb
-  test("renders without error", () => {
-    const { container } = render(<Breadcrumb data={[]}/>);
-    expect(true).toEqual(true);
-  });
+    test("renders without error", () => {
+      const { container } = render(<Breadcrumb data={[
+        { id: "link-1", href: "/", text: "Home" },
+        { id: "link-2", href: "/", text: "Menu" },
+        { id: "link-3", href: "/", text: "Profile" },
+      ] }/>);
+      expect(container).toMatchSnapshot();
+    });
 });
